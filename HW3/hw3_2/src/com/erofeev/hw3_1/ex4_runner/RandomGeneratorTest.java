8c2b34a1833164cfb1ce6e74d43a7b49bba1989c
Alexei Erofeev <erofeevas152@gmail.com>
2017-07-24 18:43:38 +0600

    HW3_2_4

---
+++ HW3/hw3_2/src/com/erofeev/hw3_1/ex4_runner/RandomGeneratorTest.java
@@ -0,0 +1,15 @@
+package com.erofeev.hw3_1.ex4_runner;
+
+import com.erofeev.hw3_2.ex4.NumberGenerator;
+
+public class RandomGeneratorTest {
+
+	public static void main(String[] args) {
+		
+		NumberGenerator  nubmergenerator = new NumberGenerator();
+	
+		nubmergenerator.sumDigit();
+
+	}
+
+}
