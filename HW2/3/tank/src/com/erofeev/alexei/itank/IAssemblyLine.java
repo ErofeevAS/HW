d84045a5fb52f4824ebfa16796752f74edd199b5
Alexei Erofeev <erofeevas152@gmail.com>
2017-08-01 17:29:47 +0300

    delete

--- HW2/3/tank/src/com/erofeev/alexei/itank/IAssemblyLine.java
+++
@@ -1,6 +0,0 @@
-package com.erofeev.alexei.itank;
-
-public interface IAssemblyLine {
-	IProduct assembleProduct(IProduct p);
-
-}
--- HW2/3/tank/src/com/erofeev/alexei/itank/ILineStep.java
+++
@@ -1,6 +0,0 @@
-package com.erofeev.alexei.itank;
-
-public interface ILineStep {
-	IProductPart buildProductPart();
-
-}
